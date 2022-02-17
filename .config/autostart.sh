fc791b03b156e8ecb659b4f752093ab5fedc2020
Manuel Díaz <ceo@macnolo.net>
2022-02-17 12:17:41 -0600

    Using Pipewire instead of Pulseaudio

--- .config/autostart.sh
+++ .config/autostart.sh
@@ -7,3 +7,7 @@ dunst &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 urxvtd -q &
 emacs --daemon &
+
+/usr/bin/pipewire &
+/usr/bin/pipewire-pulse &
+/usr/bin/pipewire-media-session &
